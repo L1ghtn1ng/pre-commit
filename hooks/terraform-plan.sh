c2c2cb9097a080b18a7d5010d733f546c13b785b
J.Townsend <L1ghtn1ng@users.noreply.github.com>
2021-08-11 18:58:52 +0100

    Update terraform-plan.sh

--- hooks/terraform-plan.sh
+++ hooks/terraform-plan.sh
@@ -6,10 +6,6 @@ set -e
 # especially as Apple changes the GUI app behavior every release (see https://stackoverflow.com/q/135688/483528). As a
 # workaround to allow GitHub Desktop to work, add this (hopefully harmless) setting here.
 export PATH=$PATH:/usr/local/bin
+export TF_IN_AUTOMATION=1
 
-# Store and return last failure from fmt so this can validate every directory passed before exiting
-PLAN_ERROR=0
-
-terraform plan || PLAN_ERROR=$?
-
-exit ${PLAN_ERROR}
+terraform plan -detailed-exitcode
